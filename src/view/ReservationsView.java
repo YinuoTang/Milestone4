4d232c06767c62d23c42387041982cdecbd51541
YinuoTang <71058600+YinuoTang@users.noreply.github.com>
2020-12-06 22:49:25 -0800

    Update ReservationsView.java

--- src/view/ReservationsView.java
+++ src/view/ReservationsView.java
@@ -1,4 +1,31 @@
 package view;
 
 public class ReservationsView {
+  // 3 buttons 
+  JButton Reservation_1  = new JButton("Reservation 1");
+	add(Reservation_1);
+	JButton Reservation_2 = new JButton ("Reservation 2");
+	add(Reservation_2);
+	JButton goBack = new JButton("Go Back");
+	add(goBack);
+	//basic set up
+	setVisible(true);
+  setSize(500, 500);
+  setLayout(new FlowLayout());
+  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  
+  Reservation_1.addActionListener(event -> {
+            try {
+                this.queue.put(new BookMessage()); // add Book message to queue
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+   Reservation_2.addActionListener(event -> {
+            try {
+                this.queue.put(new ReservationsMessage()); // add Reservation message to queue
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 }
