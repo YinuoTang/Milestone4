29a2d0cfe8e2dbf60c28ef3dbba3debae6fdc74b
YinuoTang <71058600+YinuoTang@users.noreply.github.com>
2020-12-06 22:44:42 -0800

    Update ReservationsView.java

--- src/view/ReservationsView.java
+++ src/view/ReservationsView.java
@@ -1,4 +1,16 @@
 package view;
 
 public class ReservationsView {
+  // 3 buttons for this view
+		JButton Reservation_1  = new JButton("Reservation 1");
+		add(Reservation_1);
+		JButton Reservation_2 = new JButton ("Reservation 2");
+		add(Reservation_2);
+		JButton goBack = new JButton("Go Back");
+		add(goBack);
+		//basic set up
+		setVisible(true);
+    setSize(500, 500);
+    setLayout(new FlowLayout());
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
